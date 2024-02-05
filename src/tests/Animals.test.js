3299707f4b62be544d965febf019ffb0ccbff46a
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-02-05 13:48:08 -0300

    6° commit - tdd tela Animals: Existe tabela em Animals? RED

--- src/tests/Animals.test.js
+++ src/tests/Animals.test.js
@@ -20,4 +20,8 @@ describe('Testes da tela Animals', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe tabela em Animals?', () => {
+    expect(screen.getByTestId('mytable')).toBeInTheDocument();
+  });
+
 });
